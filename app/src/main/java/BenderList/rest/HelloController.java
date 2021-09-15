161d4ef32bf4a0913949a8d44ab867e6fdac89f6
Kit_EA <kit.earf@gmail.com>
2021-09-15 22:12:57 +0500

    feat: hello route

---
+++ app/src/main/java/BenderList/rest/HelloController.java
@@ -0,0 +1,13 @@
+package BenderList.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String hello() {
+        return "Hello World!";
+    }
+}
